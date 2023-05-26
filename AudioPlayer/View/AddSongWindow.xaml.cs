a07b60ca170855cf49eafd740e5c1591b3fa8f0d
CheremisinaElizaveta <isip_e.b.cheremisina@mpt.ru>
2023-05-26 21:14:10 +0300

    жоска загружать плеер работать программисты

---
+++ AudioPlayer/View/AddSongWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AudioPlayer.View
+{
+    /// <summary>
+    /// Interaction logic for AddSongWindow.xaml
+    /// </summary>
+    public partial class AddSongWindow : Window
+    {
+        public AddSongWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
---
+++ AudioPlayer/ViewModel/AboutAuthorWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace AudioPlayer
+{
+    public class AboutAuthorWindowViewModel : INotifyPropertyChanged
+    {
+        private string _AboutAuthor = "";
+        public string AboutAuthor { get { return _AboutAuthor; } set { _AboutAuthor = value; NotifyPropertyChanged("AboutAuthor"); } }
+        public ICommand OkCommand { get; set; }
+        public EventHandler? OnCloseRequest { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public AboutAuthorWindowViewModel()
+        {
+            OkCommand = new RelayCommand(closeWindow);
+            AboutAuthor = "Короче работу выполнила просто крутой чел Черемисина Елизавета ";
+        }
+
+
+        private void closeWindow()
+        {
+            OnCloseRequest(this, EventArgs.Empty);
+        }
+
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));  
+        }
+    }
+}
